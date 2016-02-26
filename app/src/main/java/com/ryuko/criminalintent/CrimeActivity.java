56ab80998feaccaee017e3451972e0193b2a3e00
ferb96 <fduy96@gmail.com>
2016-02-25 19:21:45 -0600

    sync

--- app/src/main/java/com/ryuko/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/ryuko/criminalintent/CrimeActivity.java
@@ -1,8 +1,10 @@
 package com.ryuko.criminalintent;
 
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -10,5 +12,10 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
     }
 }
