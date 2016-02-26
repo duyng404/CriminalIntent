fc1f93c2da0b1fc0ffd5dabcb02443d462302856
ferb96 <fduy96@gmail.com>
2016-02-25 22:26:33 -0600

    Finished chapter 7

--- app/src/main/java/com/ryuko/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/ryuko/criminalintent/CrimeActivity.java
@@ -1,10 +1,7 @@
 package com.ryuko.criminalintent;
 
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.*;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -12,10 +9,13 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
-    }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_crime, container, false);
-        return v;
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/ryuko/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/ryuko/criminalintent/CrimeFragment.java
@@ -1,14 +1,45 @@
 package com.ryuko.criminalintent;
 
+import android.support.v4.app.*;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
-public class CrimeFragment extends android.support.v4.app.Fragment {
+public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Dis place intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // also left blank
+            }
+        });
+        return v;
     }
 }
